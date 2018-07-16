c1c8f1e5927c33fedd0dce328f9677254360a38a
Leo Lin <leolmq@gmail.com>
2018-07-16 11:34:41 +0800

    update url

--- docs/javascripts/extra.js
+++ docs/javascripts/extra.js
@@ -1,9 +1,9 @@
 document.getElementById("select-docs-version").onchange = function (event) {
     let currentValue = document.getElementById("select-docs-version").value;
     if (currentValue === '2.x') {
-        window.location.href = 'https://docs-gl-inet.linminquan.com/2.x/';
+        window.location.href = 'https://docs.gl-inet.com/en/2/';
     }
     else if (currentValue === '3.x') {
-        window.location.href = 'https://docs-gl-inet.linminquan.com/';
+        window.location.href = 'https://docs.gl-inet.com/en/3/';
     }
 };
